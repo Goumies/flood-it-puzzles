65110c58d15cc130b42f4f8cf7865e6f5e437297
Romy Alula <goumiesone.dev@gmail.com>
2020-08-28 23:45:15 +0200

    Init project

---
+++ src/test/solution.js
@@ -0,0 +1,13 @@
+const {describe, it} = require('mocha');
+const chai = require('chai');
+
+const assert = chai.assert;
+const should = chai.should();
+
+describe ('Flood it Puzzles', () =>{
+  describe('Classic', () => {
+    it('should clear the board', () => {
+      (0).should.equal(1)
+    })
+  })
+})
